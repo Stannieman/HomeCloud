f3bd7f284b70706aecc4f7cfb711834a960a72b9
Stan Wijckmans <st.stanie@hotmail.com>
2026-06-07 10:41:39 +0200

    Add proper scripts and config file for WireGuard.

--- Components/WireGuard/start.sh
+++ Components/WireGuard/start.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
-HOME_DIR=`getent passwd "$(logname)" | cut -d: -f6`
 . /ComponentConfigs/WireGuard.hcconfig
 
-echo "$WIREGUARD_CONFIG" > /dev/shm/wireguard.conf
-wg-quick up /dev/shm/wireguard.conf
+echo "$WIREGUARD_CONFIG" > /dev/shm/wg0.conf
+wg-quick up /dev/shm/wg0.conf
