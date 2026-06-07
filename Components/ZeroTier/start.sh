4ea1416d67fe1f70b599544d499af0af429fff00
Stan Wijckmans <st.stanie@hotmail.com>
2026-06-07 10:41:39 +0200

    Wait to connect to ZeroTier until service has started.

--- Components/ZeroTier/start.sh
+++ Components/ZeroTier/start.sh
@@ -3,4 +3,10 @@
 . /ComponentConfigs/ZeroTier.hcconfig
 
 systemctl start zerotier-one
+
+while [[ $(systemctl show -p SubState --value zerotier-one) != "running" ]]
+do
+  sleep 1
+done
+
 zerotier-cli join "$ZEROTIER_NETWORK_ID"
